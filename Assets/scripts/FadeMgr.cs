b938bbe488cac6349b9d1e0d325ff46e2e5f66e5
crosslife <geziyang@foxmail.com>
2014-06-13 17:35:42 +0800

    splash

---
+++ Assets/scripts/FadeMgr.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeMgr : MonoBehaviour {
+
+    SpriteRenderer renderer ;
+	// Use this for initialization
+	void Start () {
+        //gameObject.renderer.enabled = false;
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        Color nowColor = renderer.color;
+        renderer.color =  new Color(nowColor.r, nowColor.g, nowColor.b, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Color nowColor = renderer.color;
+            renderer.color = new Color(nowColor.r, nowColor.g, nowColor.b, 0.5f);
+        }
+	}
+}
